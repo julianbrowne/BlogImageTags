39c7f8462eedc6c6623f1dedcf4c7ab070a49f0a
Julian Browne <julianbrowne@gmail.com>
2013-09-24 16:14:35 +0100

    initial commit

---
+++ BlogImageTags.rb
@@ -0,0 +1,88 @@
+#
+#
+#   Usage
+#
+#   {% blog_image_left   image [caption] %}
+#   {% blog_image_center image [caption] %}
+#   {% blog_image_right  image [caption] %}
+#
+
+module BlogImageTags
+
+    @html    = nil
+    @args    = nil
+    @image   = nil
+    @caption = nil
+
+    def image_tag_init(tag_name, markup, tokens)
+        @html    = Liquid::Template.parse("<div class=\"image-container {{class}}\"><p class=\"image-graphic\"><img src=\"{{src}}\" /></p><p class=\"image-caption\">{{caption}}</p></div>")
+        @args    = markup.scan(/(?:"(?:\\.|[^"])*"|[^" ])+/)
+        @image     = @args[0] || ""
+        @caption = @args[1] || ""
+        @caption.sub!(/^\"/,'')
+        @caption.sub!(/\"$/,'')
+    end
+
+    def image_html(justification, context)
+        attachments = context["site"]["attachments_root"]
+        root = context["page"]["url"].split('/').last.sub(/\..*?$/,'')
+        image_url = "#{attachments}/#{root}/images/#{@image}"
+        a = @html.render({ 'class' => justification, 'src' => image_url, 'caption' => @caption})
+        return a
+    end
+
+end
+
+module Jekyll
+
+    class BlogImageLeft < Liquid::Tag
+
+        include BlogImageTags
+
+        def initialize(tag_name, markup, tokens)
+            image_tag_init(tag_name, markup, tokens)
+            super
+        end
+
+        def render(context)
+            image_html("image-left", context)
+        end
+
+    end
+
+    class BlogImageCenter < Liquid::Tag
+
+        include BlogImageTags
+
+        def initialize(tag_name, markup, tokens)
+            image_tag_init(tag_name, markup, tokens)
+            super
+        end
+
+        def render(context)
+            image_html("image-center", context)
+        end
+
+    end
+
+    class BlogImageRight < Liquid::Tag
+
+        include BlogImageTags
+
+        def initialize(tag_name, markup, tokens)
+            image_tag_init(tag_name, markup, tokens)
+            super
+        end
+
+        def render(context)
+            
+            image_html("image-right", context)
+        end
+
+    end
+
+end
+
+Liquid::Template.register_tag('blog_image_left',   Jekyll::BlogImageLeft)
+Liquid::Template.register_tag('blog_image_center', Jekyll::BlogImageCenter)
+Liquid::Template.register_tag('blog_image_right',  Jekyll::BlogImageRight)
--- README.md
+++ README.md
@@ -2,3 +2,44 @@ BlogImageTags
 =============
 
 Jekyll Liquid tags plugin that generates blog images with optional captions
+
+### Usage
+
+Add BlogImageTags to the \_plugins directory of Jekyll
+
+Embed tags along with markdown etc like so:
+
+> {% {image\_tag} {image\_name} [caption] %}
+
+Supports three {image\_tag} variants: blog\_image\_left, blog\_image\_center, blog\_image\_right
+
+Examples:
+
+    {% blog_image_right my_image.png "This is my image" %}
+
+    {% blog_image_right my_image.png %}
+
+    {% blog_image_center another_image.png "An here's the caption" %}
+
+### Output
+
+    <div class="image-container image-center">
+        <p class="image-graphic">
+            <img src="/attachments/{blog_post_name}/images/{image_name}">
+        </p>
+        <p class="image-caption">
+        </p>
+    </div>
+
+Which can be styled with CSS as required (an example is included).   
+
+image\_name is the same as the image\_name supplied in the tag   
+
+blog\_post\_name is extracted from the url the blog post is being saved to. For example:
+
+    /somewhere/something/abc_xyz_post.html
+
+Results in a blog\_post\_name of abc\_xyz\_post. If the image was called "fred.jpg" then the URL link generated would be:
+
+    /attachments/abc_xyz_post/images/fred.jpg
+
